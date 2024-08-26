1069402cd97abbeee668b4188052431f20e32c1f
thezulux24 <thezulux24gmail.com>
2024-08-26 11:43:09 -0500

    al

--- encrypt.py
+++ encrypt.py
@@ -2,13 +2,13 @@ from cryptography.fernet import Fernet
 import base64
 
 # Clave de cifrado proporcionada
-clave_original = '0'
+clave_original = "0"
 clave = base64.urlsafe_b64encode(clave_original.ljust(32)[:32].encode())
 
 cipher_suite = Fernet(clave)
 
 # Leer el contenido del archivo main.py
-with open('main.py', 'rb') as file:
+with open('maina.py', 'rb') as file:
     file_data = file.read()
 
 # Encriptar el contenido del archivo
